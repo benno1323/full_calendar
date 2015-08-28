f9645cc4b8cc2f16cf78238055eefe0b7999e668
Benito Miranda <benomir82@gmail.com>
2015-08-27 20:03:13 -0500

    Set root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
   resources :events
   # The priority is based upon order of creation: first created -> highest priority.
